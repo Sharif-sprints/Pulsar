0b8a61afecf9503ea29e23d59a0853cf9d02d0e3
AhmedrAshraf <ahmedr.0331@gmail.com>
2025-02-22 02:59:37 +0500

    minor fixing for api

--- vite.config.ts
+++ vite.config.ts
@@ -5,5 +5,9 @@ export default defineConfig({
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
-  }
+  },
+  base: "./", // Ensures relative paths for assets
+  build: {
+    outDir: "dist", // Vercel serves from 'dist'
+  },
 });
